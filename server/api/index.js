f13bf1324d3156db65c16410fddc9aba938b2363
whiteststar <48384895+whiteststar@users.noreply.github.com>
2019-09-15 15:07:28 +0200

    sql connection

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,30 @@
 const apiRouter = require('express').Router();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+// parse application/json
+apiRouter.use(bodyParser.json())
 
 apiRouter.get('/', function(req, res) {
     res.send('triggered by GET /api/ path');
 });
 
-apiRouter.post('/add', function(req, res) {
-    res.send('triggered by POST /api/add path');
+apiRouter.post('/message/add', function(req, res) {
+    console.log(req.body);
+    res.send(req.body);
+
+    res.send('triggered by POST /api/');
 });
+
+
+
+// Application initialization
+
+var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : 'class4project',
+    });
 
 module.exports = apiRouter;
--- server/app.js
+++ server/app.js
@@ -7,16 +7,20 @@ const app = express(); // Creating an Express instance
 
 app.use('*', function logGetRequests(req, res, next) {
     // eslint-disable-next-line no-console
-    console.log('someone made a request with GET method');
+    console.log('someone made a request with another method');
     next();
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api', apiRouter);
+app.use('/api', apiRouter);
 
 app.get('/', function(req, res) {
     res.send('index page, triggered by GET /');
+});
+
+app.get('*', function(req, res) {
+    res.status(404).send('Not found');
 });
 
 module.exports = app;
